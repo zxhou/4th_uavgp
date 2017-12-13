39beff6ac6f64a1fd9d3f972b875234726240a73
zxhou <zxhou2016@gmail.com>
2017-12-13 15:33:54 +0800

    create a license

--- zxVisualDetection.cpp
+++ zxVisualDetection.cpp
@@ -1,4 +1,22 @@
+/************************************************************************
+  	This file is part of 4th_uavgp(visual detection part) 
+    Copyright (C) 2016  Zhixing Hou <zxhou2016@gmail.com>
+    (Intelligent Mobile Robotics Lab (iMRL),
+    Nanjing University of Science and Technology, China)
 
+    4th_uavgp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    4th_uavgp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*******************************************************************************/
 
 #include "zxVisualDetection.h"
 using namespace std;
